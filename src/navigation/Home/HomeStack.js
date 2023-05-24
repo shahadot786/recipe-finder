e0bb8740584a8883fb198309abe92af0b57be125
MD. Shahadot Hossain <shrhossain786@gmail.com>
2023-05-24 14:48:39 +0600

    start auth

--- src/navigation/Home/HomeStack.js
+++ src/navigation/Home/HomeStack.js
@@ -1,4 +1,3 @@
-import { View, Text } from "react-native";
 import React from "react";
 import strings from "../../strings";
 import HomeScreen from "../../screens/Home/HomeScreen";
--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -1,4 +1,3 @@
-import { View, Text } from "react-native";
 import React, { useState } from "react";
 import AuthStack from "./Auth/AuthStack";
 import HomeStack from "./Home/HomeStack";
